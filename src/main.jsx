f0d2c2d4f569308f6410f3112d14eeaf231b1808
CrisGMO <cristiancampos1508@gmail.com>
2024-05-29 11:41:22 -0300

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM, { createRoot } from 'react-dom/client'
 import { Carousel } from "antd";
+import { Analytics } from "@vercel/analytics/react"
 
 const carruselRoot = ReactDOM.createRoot(document.getElementById('carrusel'));
+const analytics = ReactDOM.createRoot(document.getElementById('analytics'));
+analytics.render(<Analytics></Analytics>)
 const contentStyle = {
     margin: 0,
     height: '160px',
@@ -11,42 +14,7 @@ const contentStyle = {
     textAlign: 'center',
     background: '#364d79',
   };
-const imagenes = [ ,
-                    ,
-                    ,
-                    ];
 
-// carruselRoot.render(
-//     <Carousel className='carrusel' dots={false} infinite fade>
-//         <div className='carrusel-interior'>
-//             <img src={imagenes[0]} alt="" />
-//         </div>
-//         <div className='carrusel-interior'>
-//             <img src={imagenes[1]} alt="" />
-//         </div>
-//         <div className='carrusel-interior'>
-//             <img src={imagenes[2]} alt="" />
-//         </div>
-//         <div className='carrusel-interior'>
-//             <img src={imagenes[3]} alt="" />
-//         </div>
-//     </Carousel>
-// )
-
-    // <Carousel className='carrusel'>
-    //     <div>
-    //         <img src="https://i.imgur.com/DJzIHIr.png" alt="" />
-    //     </div>
-    //     <div>
-    //         <img src="https://i.imgur.com/lJD0TT6.png" alt="" />
-    //     </div>
-    //     <div>
-    //         <img src="https://i.imgur.com/uqkXL6g.png" alt="" />
-    //     </div>
-    //     <div>
-    //         <img src="https://i.imgur.com/eu9zYij.png" alt="" />
-    //     </div>
-    // </Carousel>
 carruselRoot.render(
     <Carousel autoplay fade>
     <div>
